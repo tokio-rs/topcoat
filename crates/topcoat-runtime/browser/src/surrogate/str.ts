28d0fc9e245fd8aded9c1977103e9a1023e35f40
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2026-06-08 16:44:51 +0200

    Add common Str methods

--- crates/topcoat-runtime/browser/src/surrogate/str.ts
+++ crates/topcoat-runtime/browser/src/surrogate/str.ts
@@ -1,4 +1,5 @@
 import { Bool } from "./bool";
+import { F64 } from "./f64";
 
 export class Str {
 	constructor(protected readonly v: string) {}
@@ -25,6 +26,38 @@ export class Str {
 
 	le(other: Str): Bool {
 		return new Bool(this.v <= other.v);
+	}
+
+	is_empty(): Bool {
+		return new Bool(this.v.length === 0);
+	}
+
+	len(): F64 {
+		return new F64(this.v.length);
+	}
+
+	trim(): Str {
+		return new Str(this.v.trim());
+	}
+
+	trim_start(): Str {
+		return new Str(this.v.trimStart());
+	}
+
+	trim_end(): Str {
+		return new Str(this.v.trimEnd());
+	}
+
+	starts_with(other: Str): Bool {
+		return new Bool(this.v.startsWith(other.v));
+	}
+
+	ends_with(other: Str): Bool {
+		return new Bool(this.v.endsWith(other.v));
+	}
+
+	contains(other: Str): Bool {
+		return new Bool(this.v.includes(other.v));
 	}
 
 	toJSON(): { t: "str" | "String"; v: string } {
--- crates/topcoat-runtime/src/runtime/surrogate/_str.rs
+++ crates/topcoat-runtime/src/runtime/surrogate/_str.rs
@@ -1,6 +1,6 @@
 use ref_cast::RefCast;
 
-use crate::runtime::{Bool, impl_surrogate_mut, impl_surrogate_ref, serialize_tagged};
+use crate::runtime::{Bool, F64, impl_surrogate_mut, impl_surrogate_ref, serialize_tagged};
 
 #[derive(Debug, RefCast)]
 #[repr(transparent)]
@@ -41,3 +41,45 @@ impl_cmp_op!(gt, >);
 impl_cmp_op!(lt, <);
 impl_cmp_op!(ge, >=);
 impl_cmp_op!(le, <=);
+
+impl Str {
+    #[inline]
+    pub fn is_empty(&self) -> Bool {
+        Bool::new(self.0.is_empty())
+    }
+
+    #[inline]
+    pub fn len(&self) -> F64 {
+        F64::new(self.0.len() as f64)
+    }
+
+    #[inline]
+    pub fn trim(&self) -> &Str {
+        Str::ref_cast(self.0.trim())
+    }
+
+    #[inline]
+    pub fn trim_start(&self) -> &Str {
+        Str::ref_cast(self.0.trim_start())
+    }
+
+    #[inline]
+    pub fn trim_end(&self) -> &Str {
+        Str::ref_cast(self.0.trim_end())
+    }
+
+    #[inline]
+    pub fn starts_with(&self, other: &Str) -> Bool {
+        Bool::new(self.0.starts_with(&other.0))
+    }
+
+    #[inline]
+    pub fn ends_with(&self, other: &Str) -> Bool {
+        Bool::new(self.0.ends_with(&other.0))
+    }
+
+    #[inline]
+    pub fn contains(&self, other: &Str) -> Bool {
+        Bool::new(self.0.contains(&other.0))
+    }
+}
