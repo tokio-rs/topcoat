d142fd3eea2fe450056e5384e3f0b81bb58c2134
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2026-06-08 18:57:15 +0200

    Fix CI

--- crates/topcoat/src/runtime.rs
+++ crates/topcoat/src/runtime.rs
@@ -1,11 +1,10 @@
 pub use topcoat_runtime::runtime::*;
 pub use topcoat_runtime_macro::expr;
 
-use topcoat::view::{component, view};
-
-#[component]
+#[cfg(feature = "view")]
+#[topcoat::view::component]
 pub async fn script() -> topcoat::Result {
-    view! {
+    topcoat::view::view! {
         <script type="module" src=(topcoat::runtime::SCRIPT)></script>
     }
 }
