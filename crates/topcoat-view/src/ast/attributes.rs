2200a0438e2e83455fb81d2763d8e72a564ba25d
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2026-07-10 18:55:55 +0200

    Error on duplicate attributes

--- crates/topcoat-view/src/ast/attributes.rs
+++ crates/topcoat-view/src/ast/attributes.rs
@@ -7,6 +7,9 @@ mod attribute_value;
 mod attribute_writer;
 mod bind_attribute;
 mod event_handler;
+mod visitor;
+
+use std::collections::HashSet;
 
 pub use attribute::*;
 pub use attribute_key::*;
@@ -17,14 +20,16 @@ pub use attribute_value::*;
 pub(crate) use attribute_writer::*;
 pub use bind_attribute::*;
 pub use event_handler::*;
+pub use visitor::*;
 
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
 use quote::{ToTokens, quote};
 use syn::Ident;
 use syn::parse::{Parse, ParseStream};
 
 use topcoat_core::ast::ParseOption;
 
+use crate::ast::template::{TemplateForLoop, TemplateIf, TemplateMatch};
 use crate::ast::view::{ViewWriter, WriteView, parse_leading_cx};
 
 /// The full list of attributes attached to a single tag.
@@ -64,6 +69,84 @@ impl WriteAttribute for Attributes {
     }
 }
 
+impl Parse for Attributes {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let cx = parse_leading_cx(input);
+        let mut items = Vec::new();
+        while let Some(item) = input.call(AttributeNode::parse_option)? {
+            items.push(item);
+        }
+
+        // Check for uniqueness of attribute keys.
+        {
+            #[derive(Default)]
+            struct Visitor {
+                attributes: HashSet<String>,
+                error: Option<syn::Error>,
+            }
+
+            impl Visitor {
+                fn register(&mut self, key: String, span: Span) {
+                    if self.error.is_some() {
+                        return;
+                    }
+                    if self.attributes.contains(&key) {
+                        self.error = Some(syn::Error::new(
+                            span,
+                            format!("duplicate attribute `{key}`"),
+                        ));
+                    }
+                    self.attributes.insert(key);
+                }
+            }
+
+            impl<'ast> Visit<'ast> for Visitor {
+                fn visit_attribute(&mut self, node: &'ast Attribute) {
+                    if let Some(ident) = node.key.as_ident() {
+                        self.register(ident.to_string(), ident.span());
+                    }
+                }
+
+                fn visit_bind_attribute(&mut self, node: &'ast BindAttribute) {
+                    if let Some(ident) = node.key.as_ident() {
+                        self.register(format!(":{ident}"), ident.span());
+                    }
+                }
+
+                fn visit_event_handler(&mut self, node: &'ast EventHandler) {
+                    if let Some(ident) = node.key.as_ident() {
+                        self.register(format!("@{ident}"), ident.span());
+                    }
+                }
+
+                fn visit_for_loop(&mut self, _node: &'ast TemplateForLoop<AttributeNodes>) {
+                    // We cannot statically assert that a for loop only creates a key at most once.
+                }
+
+                fn visit_if(&mut self, _node: &'ast TemplateIf<AttributeNodes>) {
+                    // We cannot statically assert that a combination of multiple conditionals
+                    // create a key at most once.
+                }
+
+                fn visit_match(&mut self, _node: &'ast TemplateMatch<AttributeNode>) {
+                    // We cannot statically assert that a combination of multiple conditionals
+                    // create a key at most once.
+                }
+            }
+
+            let mut visitor = Visitor::default();
+            for item in &items {
+                visitor.visit_node(item);
+            }
+            if let Some(error) = visitor.error {
+                return Err(error);
+            }
+        }
+
+        Ok(Self { cx, items })
+    }
+}
+
 impl ToTokens for Attributes {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let mut writer = AttributeWriter::new();
@@ -82,17 +165,6 @@ impl ToTokens for Attributes {
             .to_tokens(tokens),
             None => attrs.to_tokens(tokens),
         }
-    }
-}
-
-impl Parse for Attributes {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let cx = parse_leading_cx(input);
-        let mut items = Vec::new();
-        while let Some(item) = input.call(AttributeNode::parse_option)? {
-            items.push(item);
-        }
-        Ok(Self { cx, items })
     }
 }
 
@@ -161,5 +233,23 @@ mod tests {
         assert!(matches!(attrs.items[1], AttributeNode::If(_)));
         assert!(matches!(attrs.items[2], AttributeNode::ForLoop(_)));
         assert!(matches!(attrs.items[3], AttributeNode::Match(_)));
+    }
+
+    #[test]
+    #[should_panic(expected = "duplicate attribute `dup-attr`")]
+    fn errors_on_duplicate_attribute() {
+        let _ = parse(r#"not-dup="5" dup-attr="6" dup-attr="7" another="8""#);
+    }
+
+    #[test]
+    #[should_panic(expected = "duplicate attribute `:dup-attr`")]
+    fn errors_on_duplicate_bind_attribute() {
+        let _ = parse(r":not-dup=(()) :dup-attr=(()) :dup-attr=(()) :another=(())");
+    }
+
+    #[test]
+    #[should_panic(expected = "duplicate attribute `@dup-attr`")]
+    fn errors_on_duplicate_event_handler() {
+        let _ = parse(r"@not-dup=(()) @dup-attr=(()) @dup-attr=(()) @another=(())");
     }
 }
---
+++ crates/topcoat-view/src/ast/attributes/visitor.rs
@@ -0,0 +1,149 @@
+use crate::ast::template::{
+    TemplateBlock, TemplateBreak, TemplateContinue, TemplateElse, TemplateForLoop, TemplateIf,
+    TemplateLet, TemplateMatch,
+};
+
+use super::*;
+
+pub trait Visit<'ast> {
+    fn visit_node(&mut self, node: &'ast AttributeNode) {
+        visit_node(self, node);
+    }
+
+    fn visit_attribute(&mut self, node: &'ast Attribute) {
+        visit_attribute(self, node);
+    }
+
+    fn visit_spread(&mut self, node: &'ast AttributeSpread) {
+        visit_spread(self, node);
+    }
+
+    fn visit_bind_attribute(&mut self, node: &'ast BindAttribute) {
+        visit_bind_attribute(self, node);
+    }
+
+    fn visit_event_handler(&mut self, node: &'ast EventHandler) {
+        visit_event_handler(self, node);
+    }
+
+    fn visit_if(&mut self, node: &'ast TemplateIf<AttributeNodes>) {
+        visit_if(self, node);
+    }
+
+    fn visit_else(&mut self, node: &'ast TemplateElse<AttributeNodes>) {
+        visit_else(self, node);
+    }
+
+    fn visit_let(&mut self, node: &'ast TemplateLet) {
+        visit_let(self, node);
+    }
+
+    fn visit_for_loop(&mut self, node: &'ast TemplateForLoop<AttributeNodes>) {
+        visit_for_loop(self, node);
+    }
+
+    fn visit_continue(&mut self, node: &'ast TemplateContinue) {
+        visit_continue(self, node);
+    }
+
+    fn visit_break(&mut self, node: &'ast TemplateBreak) {
+        visit_break(self, node);
+    }
+
+    fn visit_match(&mut self, node: &'ast TemplateMatch<AttributeNode>) {
+        visit_match(self, node);
+    }
+
+    fn visit_block(&mut self, node: &'ast TemplateBlock<AttributeNodes>) {
+        visit_block(self, node);
+    }
+}
+
+pub fn visit_node<'ast>(visit: &mut (impl Visit<'ast> + ?Sized), node: &'ast AttributeNode) {
+    match node {
+        AttributeNode::Attribute(inner) => visit.visit_attribute(inner),
+        AttributeNode::Spread(inner) => visit.visit_spread(inner),
+        AttributeNode::BindAttribute(inner) => visit.visit_bind_attribute(inner),
+        AttributeNode::EventHandler(inner) => visit.visit_event_handler(inner),
+        AttributeNode::If(inner) => visit.visit_if(inner),
+        AttributeNode::Let(inner) => visit.visit_let(inner),
+        AttributeNode::ForLoop(inner) => visit.visit_for_loop(inner),
+        AttributeNode::Continue(inner) => visit.visit_continue(inner),
+        AttributeNode::Break(inner) => visit.visit_break(inner),
+        AttributeNode::Match(inner) => visit.visit_match(inner),
+        AttributeNode::Block(inner) => visit.visit_block(inner),
+    }
+}
+
+pub fn visit_attribute<'ast>(_visit: &mut (impl Visit<'ast> + ?Sized), _node: &'ast Attribute) {}
+
+pub fn visit_spread<'ast>(_visit: &mut (impl Visit<'ast> + ?Sized), _node: &'ast AttributeSpread) {}
+
+pub fn visit_bind_attribute<'ast>(
+    _visit: &mut (impl Visit<'ast> + ?Sized),
+    _node: &'ast BindAttribute,
+) {
+}
+
+pub fn visit_event_handler<'ast>(
+    _visit: &mut (impl Visit<'ast> + ?Sized),
+    _node: &'ast EventHandler,
+) {
+}
+
+pub fn visit_if<'ast>(
+    visit: &mut (impl Visit<'ast> + ?Sized),
+    node: &'ast TemplateIf<AttributeNodes>,
+) {
+    for node in &node.then_branch.children {
+        visit.visit_node(node);
+    }
+    if let Some(else_branch) = &node.else_branch {
+        visit.visit_else(else_branch);
+    }
+}
+
+pub fn visit_else<'ast>(
+    visit: &mut (impl Visit<'ast> + ?Sized),
+    node: &'ast TemplateElse<AttributeNodes>,
+) {
+    match node {
+        TemplateElse::ElseIf { template_if, .. } => visit.visit_if(template_if),
+        TemplateElse::Else { then_branch, .. } => visit_block(visit, then_branch),
+    }
+}
+
+pub fn visit_let<'ast>(_visit: &mut (impl Visit<'ast> + ?Sized), _node: &'ast TemplateLet) {}
+
+pub fn visit_for_loop<'ast>(
+    visit: &mut (impl Visit<'ast> + ?Sized),
+    node: &'ast TemplateForLoop<AttributeNodes>,
+) {
+    visit_block(visit, &node.body)
+}
+
+pub fn visit_continue<'ast>(
+    _visit: &mut (impl Visit<'ast> + ?Sized),
+    _node: &'ast TemplateContinue,
+) {
+}
+
+pub fn visit_break<'ast>(_visit: &mut (impl Visit<'ast> + ?Sized), _node: &'ast TemplateBreak) {}
+
+pub fn visit_match<'ast>(
+    visit: &mut (impl Visit<'ast> + ?Sized),
+    node: &'ast TemplateMatch<AttributeNode>,
+) {
+    for arm in &node.arms {
+        visit.visit_node(&arm.body);
+    }
+}
+
+pub fn visit_block<'ast>(
+    visit: &mut (impl Visit<'ast> + ?Sized),
+    node: &'ast TemplateBlock<AttributeNodes>,
+) {
+    for node in &node.children {
+        visit.visit_node(node);
+    }
+}
