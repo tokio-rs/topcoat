2483a2e21d87443da57923e4dca1ad28f7f32674
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2026-07-24 17:26:46 +0200

    improve asset error message

--- crates/topcoat-asset/src/view.rs
+++ crates/topcoat-asset/src/view.rs
@@ -1,11 +1,14 @@
-use topcoat_core::context::Cx;
+use topcoat_core::context::{Cx, try_app_context};
 use topcoat_view::{AttributeValueViewParts, DynViewPart, HtmlWriter, PartsWriter};
 
-use crate::{Asset, asset_config};
+use crate::{Asset, AssetConfig};
 
 impl DynViewPart for Asset {
     fn render(&self, cx: &Cx, w: &mut HtmlWriter<'_, '_>) {
-        let _ = asset_config(cx).fmt_url(*self, w);
+        let asset_config = try_app_context::<AssetConfig>(cx).unwrap_or_else(|| {
+            panic!("no asset config registered in this router context; load the asset bundle with `.assets(AssetBundle::load().unwrap())`");
+        });
+        let _ = asset_config.fmt_url(*self, w);
     }
 
     #[inline]
