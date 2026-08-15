2461d028f9097b783cf83002ab7d12ee81ff756a
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2026-08-15 15:24:13 +0200

    add href target impl for path

--- crates/topcoat-router/src/href.rs
+++ crates/topcoat-router/src/href.rs
@@ -5,3 +5,9 @@ use crate::Path;
 pub trait HrefTarget {
     fn path<'cx>(&self, cx: &'cx Cx) -> &'cx Path;
 }
+
+impl HrefTarget for &'static Path {
+    fn path<'cx>(&self, _cx: &'cx Cx) -> &'cx Path {
+        self
+    }
+}
