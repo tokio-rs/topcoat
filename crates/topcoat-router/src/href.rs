687786ab9ac3c05ef0cfbbcf95c3961da806aa44
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2026-08-17 16:22:16 +0200

    wip

--- crates/topcoat-router/src/href.rs
+++ crates/topcoat-router/src/href.rs
@@ -1,6 +1,11 @@
-use std::fmt::Display;
+use std::fmt::{Display, Write};
 
-use topcoat_core::context::Cx;
+use serde::Serialize;
+use topcoat_core::{
+    base_url::base_url,
+    context::Cx,
+    url_form::{UrlForm, url_form},
+};
 
 use crate::Path;
 
@@ -25,4 +30,67 @@ pub trait HrefParam {
 
     fn name(&self) -> &str;
     fn value(&self) -> &Self::Value;
+}
+
+impl<T> HrefParam for &T
+where
+    T: HrefParam,
+{
+    type Value = T::Value;
+
+    fn name(&self) -> &str {
+        (*self).name()
+    }
+
+    fn value(&self) -> &Self::Value {
+        (*self).value()
+    }
+}
+
+pub trait HrefParams {
+    fn assign(&self, path: &Path, out: &mut String);
+}
+
+pub fn href<T, P>(target: T, params: P) -> Href<T, P, ()>
+where
+    T: HrefTarget,
+    P: HrefParams,
+{
+    Href {
+        target,
+        params,
+        query: (),
+        fragment: String::new(),
+    }
+}
+
+pub struct Href<T, P, Q, F> {
+    target: T,
+    params: P,
+    query: Q,
+    fragment: Option<F>,
+}
+
+impl<T, P, Q, F> Href<T, P, Q, F>
+where
+    T: HrefTarget,
+    P: HrefParams,
+    Q: Serialize,
+    F: Display,
+{
+    pub fn resolve(self, cx: &Cx) -> String {
+        let mut buf = String::new();
+        match url_form(cx) {
+            UrlForm::Absolute => buf += base_url(cx).as_str(),
+            UrlForm::Relative => {}
+        }
+
+        self.params.assign(self.target.path(cx), &mut buf);
+
+        if let Some(fragment) = self.fragment {
+            write!(buf, "#{fragment}");
+        }
+
+        self.buf
+    }
 }
